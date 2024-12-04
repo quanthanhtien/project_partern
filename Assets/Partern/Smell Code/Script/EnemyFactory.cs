6fb4bd469c6a4333db51caadbbe8182118fd253e
quan ng <anhquan2712003@gmail.com.com>
2024-12-05 00:51:40 +0700

    SmellCode

---
+++ Assets/Partern/Smell Code/Script/EnemyFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Smell
+{
+    public class EnemyFactory : IEnemyFactory
+    {
+        public Enemy Create(EnemyConfig config)
+        {
+            {
+                GameObject enemy = Object.Instantiate(config.Prefab);
+                Enemy enemyComponent = enemy.GetComponent<Enemy>();
+                enemyComponent.Initialize(config);
+                return enemyComponent;
+            }
+        }
+    }
+}
---
+++ Assets/Partern/Smell Code/Script/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Smell
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        public int maxEnemies = 10;
+        public List<EnemyConfig> enemyConfigs;
+        EnemyFactory enemyFactory = new EnemyFactory();
+
+        private void Start()
+        {
+            SpawnEnemy();
+        }
+
+        public void SpawnEnemy()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Enemy enemy = enemyFactory.Create(enemyConfigs[i % enemyConfigs.Count]);
+                enemy.transform.position = new Vector3(i * 2.0f, 0, 0);
+            }
+        }
+    }
+
+    public abstract class PlacementStrategy
+    {
+        public abstract void SetPosition(Enemy enemy, int index);
+    }
+}
